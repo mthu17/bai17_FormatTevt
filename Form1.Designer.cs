529fc0b5d9e3364c226dc8d81f612714155bd563
SPC <SPC@23-LAB402>
2025-03-28 09:36:04 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace bai17_FormatTevt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radRed = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radGrenn = new System.Windows.Forms.RadioButton();
+            this.radBlue = new System.Windows.Forms.RadioButton();
+            this.radBlack = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chkBold = new System.Windows.Forms.CheckBox();
+            this.chkNghiem = new System.Windows.Forms.CheckBox();
+            this.chkGach = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NHẬP TÊN";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(166, 37);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // radRed
+            // 
+            this.radRed.AutoSize = true;
+            this.radRed.Location = new System.Drawing.Point(39, 29);
+            this.radRed.Name = "radRed";
+            this.radRed.Size = new System.Drawing.Size(45, 17);
+            this.radRed.TabIndex = 2;
+            this.radRed.TabStop = true;
+            this.radRed.Text = "Red";
+            this.radRed.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radBlack);
+            this.groupBox1.Controls.Add(this.radBlue);
+            this.groupBox1.Controls.Add(this.radGrenn);
+            this.groupBox1.Controls.Add(this.radRed);
+            this.groupBox1.Location = new System.Drawing.Point(41, 116);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(184, 163);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COLOR";
+           
+            // 
+            // radGrenn
+            // 
+            this.radGrenn.AutoSize = true;
+            this.radGrenn.Location = new System.Drawing.Point(39, 52);
+            this.radGrenn.Name = "radGrenn";
+            this.radGrenn.Size = new System.Drawing.Size(54, 17);
+            this.radGrenn.TabIndex = 2;
+            this.radGrenn.TabStop = true;
+            this.radGrenn.Text = "Green";
+            this.radGrenn.UseVisualStyleBackColor = true;
+            // 
+            // radBlue
+            // 
+            this.radBlue.AutoSize = true;
+            this.radBlue.Location = new System.Drawing.Point(39, 75);
+            this.radBlue.Name = "radBlue";
+            this.radBlue.Size = new System.Drawing.Size(46, 17);
+            this.radBlue.TabIndex = 2;
+            this.radBlue.TabStop = true;
+            this.radBlue.Text = "Blue";
+            this.radBlue.UseVisualStyleBackColor = true;
+            // 
+            // radBlack
+            // 
+            this.radBlack.AutoSize = true;
+            this.radBlack.Location = new System.Drawing.Point(39, 98);
+            this.radBlack.Name = "radBlack";
+            this.radBlack.Size = new System.Drawing.Size(52, 17);
+            this.radBlack.TabIndex = 2;
+            this.radBlack.TabStop = true;
+            this.radBlack.Text = "Black";
+            this.radBlack.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.chkGach);
+            this.groupBox2.Controls.Add(this.chkNghiem);
+            this.groupBox2.Controls.Add(this.chkBold);
+            this.groupBox2.Location = new System.Drawing.Point(353, 116);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(198, 163);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // chkBold
+            // 
+            this.chkBold.AutoSize = true;
+            this.chkBold.Location = new System.Drawing.Point(42, 29);
+            this.chkBold.Name = "chkBold";
+            this.chkBold.Size = new System.Drawing.Size(75, 17);
+            this.chkBold.TabIndex = 0;
+            this.chkBold.Text = " Đậm Bold";
+            this.chkBold.UseVisualStyleBackColor = true;
+            // 
+            // chkNghiem
+            // 
+            this.chkNghiem.AutoSize = true;
+            this.chkNghiem.Location = new System.Drawing.Point(42, 76);
+            this.chkNghiem.Name = "chkNghiem";
+            this.chkNghiem.Size = new System.Drawing.Size(86, 17);
+            this.chkNghiem.TabIndex = 0;
+            this.chkNghiem.Text = "Nghiêm italic";
+            this.chkNghiem.UseVisualStyleBackColor = true;
+            // 
+            // chkGach
+            // 
+            this.chkGach.AutoSize = true;
+            this.chkGach.Location = new System.Drawing.Point(42, 118);
+            this.chkGach.Name = "chkGach";
+            this.chkGach.Size = new System.Drawing.Size(82, 17);
+            this.chkGach.TabIndex = 0;
+            this.chkGach.Text = " Gạch chân";
+            this.chkGach.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radRed;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radGrenn;
+        private System.Windows.Forms.RadioButton radBlack;
+        private System.Windows.Forms.RadioButton radBlue;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chkGach;
+        private System.Windows.Forms.CheckBox chkNghiem;
+        private System.Windows.Forms.CheckBox chkBold;
+    }
+}
+
